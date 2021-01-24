6bf9e2dd8ddb70d8913e34653d412c626b859b61
mr-astro <xastrox@gmail.com>
2021-01-24 16:23:16 -0300

    Configuracion de barra de navegacion collapsable

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("misLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
